8e9a7562991bb215ae82e41be9b9c445f2c9433c
Joost Schuur <jschuur@jschuur.com>
2024-04-23 12:37:34 +0100

    Mobile breakpoint header/footer tweaks

--- src/components/Site/Footer.tsx
+++ src/components/Site/Footer.tsx
@@ -1,20 +1,20 @@
 import { IconBrandGithub, IconBrandThreads, IconBrandTwitter } from '@tabler/icons-react';
 export default function Footer() {
   return (
-    <footer className='flex items-center gap-2 text-xs sm:text-sm py-2 px-4 bg-white border-t border-slate-400 shadow-sm'>
-      <div className='grow'>
+    <footer className='flex items-center gap-2 text-slate-500 text-[8pt] sm:text-sm py-1 sm:py-2 px-4 bg-white border-t border-slate-400 shadow-sm'>
+      <div className='grow hidden sm:block'>
         Built with <a href='https://partykit.io'>PartyKit</a>,{' '}
         <a href='https://nextjs.org'>Next.js</a>, and <a href='https://ion.sst.dev'>SST Ion</a>.
       </div>
-      <div>By Joost Schuur</div>
+      <div className='grow sm:grow-0'>By Joost Schuur</div>
       <a href='https://threads.net/@joostschuur'>
-        <IconBrandThreads className='size-5 text-black transition hover:scale-125 ease-in-out' />
+        <IconBrandThreads className='size-4 sm:size-5 text-black transition hover:scale-125 ease-in-out' />
       </a>
       <a href='https://twitter.com/joostschuur'>
-        <IconBrandTwitter className='size-5 text-blue-400 transition hover:scale-125 ease-in-out' />
+        <IconBrandTwitter className='size-4 sm:size-5 text-blue-400 transition hover:scale-125 ease-in-out' />
       </a>
       <a href='https://github.com/jschuur'>
-        <IconBrandGithub className='size-5 text-purple-700 transition hover:scale-125 ease-in-out' />
+        <IconBrandGithub className='size-4 sm:size-5 text-purple-700 transition hover:scale-125 ease-in-out' />
       </a>
     </footer>
   );
--- src/components/Site/Header.tsx
+++ src/components/Site/Header.tsx
@@ -5,7 +5,7 @@ import Account from '@/components/Site/Account';
 export default function Home() {
   return (
     <div className='flex items-center justify-between w-full gap-2 px-4 min-h-12 sm:min-h-16 bg-gradient-to-br text-white from-blue-600 to-purple-600 mb-4 border-b-black border-b-[0.5px]'>
-      <IconMapPin2 className='size-6 sm:size-8' />
+      <IconMapPin2 className='size-6 sm:size-8 hidden sm:inline-block' />
       <h1 className='font-header font-semibold text-lg sm:text-2xl grow'>Coworking World</h1>
       <Account />
     </div>
