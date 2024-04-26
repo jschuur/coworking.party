19f518bc38216fc1c34fe88bde56e13f40428474
Joost Schuur <jschuur@jschuur.com>
2024-04-26 11:24:53 +0100

    Random logo in the header

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,9 @@
+import { UserData } from '@/lib/types';
+
 export const DEFAULT_SITENAME = 'Coworking Party';
+export const SITE_STRAPLINE = 'Have fun getting stuff done';
+export const RANDOM_LOGO_INTERVAL = 5 * 60 * 1000;
+
+export const DEFAULT_USER_DATA: UserData = {
+  tagline: null,
+};
