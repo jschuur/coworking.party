df7473fa303553ea71121b803930d53a5b155ac7
Joost Schuur <jschuur@jschuur.com>
2024-04-29 11:21:41 +0100

    Fix: Next Auth session.id constraint error

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -62,7 +62,7 @@ export const verificationTokens = sqliteTable(
   {
     identifier: text('identifier').notNull(),
     token: text('token').notNull(),
-    expires: integer('expires', { mode: 'timestamp' }).notNull(),
+    expires: integer('expires', { mode: 'timestamp_ms' }).notNull(),
   },
   (vt) => ({
     compoundKey: primaryKey({ columns: [vt.identifier, vt.token] }),
