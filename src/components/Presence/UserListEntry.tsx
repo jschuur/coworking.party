e0d768746cd79c9a3f86fe2048e100171d31da61
Joost Schuur <jschuur@jschuur.com>
2024-04-29 16:11:32 +0100

    Align timestamps in UserListEntry (via @tdrayson)

--- src/components/Presence/UserListEntry.tsx
+++ src/components/Presence/UserListEntry.tsx
@@ -10,11 +10,11 @@ type Props = {
 export default function UserListEntry({ user }: Props) {
   return (
     <div className='p-2 flex gap-2 items-center even:bg-purple-200'>
-      <div className='flex flex-row gap-2 items-top'>
+      <div className='flex flex-row gap-2 items-top grow'>
         <div>
           <Avatar user={user} className='size-8 sm:size-8' status={'online'} />
         </div>
-        <div className='flex flex-col min-w-64 max-w-72'>
+        <div className='flex flex-col min-w-64 max-w-72 grow'>
           <div className='text-lg'>{user.name}</div>
           <div className='text-sm text-slate-500 min-h-[1.4em]'>{user.tagline || ''}</div>
           {user.sessionStartedAt && (
