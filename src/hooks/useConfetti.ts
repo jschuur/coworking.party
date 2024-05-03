83dd7c10a37b63ec5abc39143c84d1ff71597184
Joost Schuur <jschuur@jschuur.com>
2024-05-03 12:27:51 +0100

    Fix: Skip confetti when page not visible

--- src/hooks/useConfetti.ts
+++ src/hooks/useConfetti.ts
@@ -1,14 +1,15 @@
-import { useAtom } from 'jotai';
+import { useAtom, useAtomValue } from 'jotai';
 import posthog from 'posthog-js';
 
 import { CONFETTI_DELAY_MIN } from '@/config';
-import { confettiConductorAtom } from '@/store';
+import { confettiConductorAtom, visibilityAtom } from '@/store';
 
 export default function useConfetti() {
   const [confettiConductor, setConfettiConductor] = useAtom(confettiConductorAtom);
+  const isVisible = useAtomValue(visibilityAtom);
 
   const shootConfetti = ({ delay = CONFETTI_DELAY_MIN, source = '(none)' } = {}) => {
-    if (!confettiConductor) return;
+    if (!confettiConductor || !isVisible) return;
 
     setTimeout(() => confettiConductor.shoot(), delay);
 
