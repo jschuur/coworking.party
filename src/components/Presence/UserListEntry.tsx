3250db71013f7b82c4f7c6855dccb80013475dcd
Joost Schuur <jschuur@jschuur.com>
2024-05-04 21:00:30 +0100

    Make update links clickable

--- src/components/Presence/UserListEntry.tsx
+++ src/components/Presence/UserListEntry.tsx
@@ -1,6 +1,7 @@
 import Avatar from '@/components/Presence/Avatar';
 import TimeAgo from '@/components/Presence/TimeAgo';
 import StatusBadge from '@/components/Site/StatusBadge';
+import Linkify from 'linkify-react';
 
 import { UserPublicData } from '@/lib/types';
 
@@ -9,18 +10,25 @@ type Props = {
 };
 
 export default function UserListEntry({ user }: Props) {
+  const linkifyOptions = {
+    target: '_blank',
+    truncate: 30,
+  };
+
   return (
     <div className='p-2 flex gap-2 items-center even:bg-purple-200'>
       <div className='flex flex-row gap-2 items-top grow'>
         <div>
           <Avatar user={user} className='size-8 sm:size-10' />
         </div>
-        <div className='flex flex-col min-w-64 max-w-72 grow'>
-          <div className='flex flex-row items-center gap-8 text-xl'>
-            <div className='flex-grow'>{user.name}</div>
+        <div className='flex flex-col grow'>
+          <div className='flex flex-row items-center gap-4 sm:gap-8 text-xl'>
+            <div className='flex-grow text-sm sm:text-base'>{user.name}</div>
             <div>{user.status !== 'online' && <StatusBadge user={user} />}</div>
           </div>
-          <div className='text-sm text-slate-500 mt-2 min-h-[1.4em]'>{user.tagline || ''}</div>
+          <div className='text-sm text-slate-500 mt-2 min-h-[1.4em]'>
+            <Linkify options={linkifyOptions}>{user.tagline || ''}</Linkify>
+          </div>
           {user.away
             ? user.awayStartedAt && (
                 <TimeAgo
